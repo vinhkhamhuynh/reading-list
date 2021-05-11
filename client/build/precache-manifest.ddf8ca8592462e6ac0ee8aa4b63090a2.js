cd1fa62a199dd6089a8fc3481e7f4f04ba68479d
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-05-10 23:58:53 -0400

    intalled all needed component and dependencies to deploy to heroku

---
+++ client/build/precache-manifest.ddf8ca8592462e6ac0ee8aa4b63090a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf05a7544d5b16bda20b708dffe3ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c384e225323bc8db2b2d",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c384e225323bc8db2b2d",
+    "url": "/static/js/main.637fc75d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
